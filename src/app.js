c33d063d8b8776a033e92ddee399857cbdf78b85
kwonz <kwonzhu@gmail.com>
2022-05-10 14:55:07 +0930

    add root handler

--- src/app.js
+++ src/app.js
@@ -15,6 +15,9 @@ app.use(cors());
 app.use(helmet());
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Welcome to deploy-test');
+});
 app.use('/api', router);
 app.use(errorHandler);
 
